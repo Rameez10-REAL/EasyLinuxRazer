a68bf2fd6ba37002f5958f8044867cc1a7b89f97
Rameez10 <Rameezmajid10@outlook.com>
2022-03-12 10:12:37 +0000

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,7 @@
+sudo apt install software-properties-gtk
+sudo add-apt-repository ppa:openrazer/stable
+sudo apt update
+sudo apt install openrazer-meta
+sudo add-apt-repository ppa:polychromatic/stable
+sudo apt update
+sudo apt install polychromatic
