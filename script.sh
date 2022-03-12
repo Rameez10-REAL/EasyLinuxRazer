a21f2a4cad4c167f25501c496e8f5918a807ff4c
Rameez10 <Rameezmajid10@outlook.com>
2022-03-12 10:19:12 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,8 @@
-sudo apt install software-properties-gtk
-sudo add-apt-repository ppa:openrazer/stable
-sudo apt update
-sudo apt install openrazer-meta
+sudo apt install software-properties-gtk -y
+sudo add-apt-repository ppa:openrazer/stable -y
+sudo apt update -y
+sudo apt install openrazer-meta -y
 sudo gpasswd -a $USER plugdev
-sudo add-apt-repository ppa:polychromatic/stable
-sudo apt update
-sudo apt install polychromatic
+sudo add-apt-repository ppa:polychromatic/stable -y
+sudo apt update -y
+sudo apt install polychromatic -y
